9997259b0c2dff594f53ee7a31637f56404cd89b
hossamabdulmagid <hosamabdulmaged@gmail.com>
2020-03-13 08:00:12 +0200

    RemovingFiles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+
+import { HomePage } from './pages/homepage/homepage.component.jsx';
 import { ShopPage } from './pages/shop/shop.component.jsx';
-import { Route, Switch } from 'react-router-dom'
-import { HatsPage } from "./pages/hatspage/hatsPage.component"
-import { Jackets } from "./pages/jacketspage/jacketspage.component"
-import { Sneakers } from "./pages/sneakerspage/sneakerspage.component"
-import { Womens } from "./pages/womenpage/womenpage.component"
-import { Mens } from "./pages/menspage/menspage.component"
+
 function App() {
   return (
-    <div >
+    <div>
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
-        <Route path="/Jackets" component={Jackets} />
-        <Route path="/Sneakers" component={Sneakers} />
-        <Route path="/Womens" component={Womens} />
-        <Route path="/mens" component={Mens} />
-        <Route path="/shop" component={ShopPage} />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,11 +2,10 @@
 import React from 'react'
 import './homepage.styles.scss'
 import Directory from '../../component/directory/directory.component'
-const HomePage = () => (
+export const HomePage = () => (
     <div className="homepage">
       <Directory />
 
     </div>
 )
 
-export default HomePage;
