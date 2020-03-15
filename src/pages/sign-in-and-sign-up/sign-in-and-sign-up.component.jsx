6b52778919967ed25d4c1edf16cd7141a5f8b5f8
hossamabdulmagid <hosamabdulmaged@gmail.com>
2020-03-15 17:37:23 +0200

    Sign up Component with scass files

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './sign-in-and-sign-up.styles.scss'
 import { SignIn } from '../../component/Sign-in/sign-in.component'
-
+import { SignUp } from '../../component/sign-up/sign-up.component'
 
 
 export const SignInAndSignUpPage = () => (
     <div className="sign-in-and-sign-up">
         <SignIn />
-            </div>
+        <SignUp />
+    </div>
 );
 
 
