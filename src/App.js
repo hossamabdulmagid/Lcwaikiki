c5839e6d8b5df34ca2acd48d4555e38411064e37
hossamabdulmagid <hosamabdulmaged@gmail.com>
2020-03-15 18:20:24 +0200

    firebase

--- src/App.js
+++ src/App.js
@@ -9,19 +9,15 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 class App extends Component {
   constructor() {
     super();
-
     this.state = {
       currentUser: null
     };
   }
-
   unsubscribeFromAuth = null;
-
   componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
-
         userRef.onSnapshot(snapShot => {
           this.setState({
             currentUser: {
@@ -29,18 +25,14 @@ class App extends Component {
               ...snapShot.data()
             }
           });
-
         });
       }
-
       this.setState({ currentUser: userAuth });
     });
   }
-
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
-
   render() {
     return (
       <div>
