bb791b1e1b26ab6e79b9f191cc66b388e156426f
hossamabdulmagid <hosamabdulmaged@gmail.com>
2020-03-16 23:10:26 +0200

    setup redux

---
+++ build/precache-manifest.eb80a582904b68f26f282747f263e8b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87744a9fc589f724b0baa7477bbf077b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca86a048f20362768717",
+    "url": "/static/css/main.84463632.chunk.css"
+  },
+  {
+    "revision": "0f4afc794620b8600291",
+    "url": "/static/js/2.87751dc6.chunk.js"
+  },
+  {
+    "revision": "9cc59aa88bbb965336535eb6b360773d",
+    "url": "/static/js/2.87751dc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca86a048f20362768717",
+    "url": "/static/js/main.a04dc1e0.chunk.js"
+  },
+  {
+    "revision": "301ab9b403b182a5f8b3",
+    "url": "/static/js/runtime-main.0219fd4c.js"
+  },
+  {
+    "revision": "143ab88868ee5de2f77953a7b60fab5d",
+    "url": "/static/media/crown.143ab888.svg"
+  }
+]);
